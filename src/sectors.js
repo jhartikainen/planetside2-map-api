53e8bcb5eb888f1b784b0a0ebb7cb9744051096d
Jani Hartikainen <jani@jjh.fi>
2013-01-24 19:25:26 +0200

    restructure

---
+++ src/sectors.js
@@ -0,0 +1,328 @@
+var sectorsIndar = [
+	{
+		name: 'Nanite Pump Depot',
+		hexes: [{"x":3,"y":0},{"x":4,"y":0},{"x":5,"y":0}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+	}, {
+		name: 'Indar Waste Treatment',
+		hexes: [{"x":6,"y":0},{"x":5,"y":1},{"x":6,"y":1},{"x":7,"y":0},{"x":8,"y":0},{"x":7,"y":1}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'NS Salvage Yard',
+		hexes: [{"x":9,"y":0},{"x":10,"y":0},{"x":11,"y":0}],
+		buildings: ['weapon_terminal', 'flash_spawn']
+		}, {
+		name: 'Archaeological Dig Site',
+		hexes: [{"x":2,"y":1}],
+		buildings: ['weapon_terminal', 'flash_spawn']
+		}, {
+		name: 'Saurva',
+		hexes: [{"x":3,"y":1},{"x":4,"y":1},{"x":3,"y":2},{"x":4,"y":2},{"x":5,"y":2},{"x":3,"y":3},{"x":4,"y":3}],
+		buildings: ['spawn_room' ,'vehicle_spawn', 'weapon_terminal']
+		}, {
+		name: 'Camp Connery',
+		hexes: [{"x":8,"y":1},{"x":8,"y":2},{"x":7,"y":3},{"x":8,"y":3},{"x":9,"y":2},{"x":9,"y":1},{"x":9,"y":3}],
+		buildings: ['weapon_terminal', 'vehicle_spawn', 'air_spawn', 'ammo_tower', 'spawn_room']
+		}, {
+		name: 'TR Warpgate',
+		hexes: [{"x":10,"y":1},{"x":10,"y":2},{"x":10,"y":3},{"x":11,"y":2},{"x":11,"y":1},{"x":12,"y":1},{"x":12,"y":2},{"x":11,"y":3}],
+		buildings: ['weapon_terminal', 'vehicle_spawn', 'air_spawn', 'spawn_room', 'ammo_tower', 'air_pad']
+		}, {
+		name: 'Alkali Storage',
+		hexes: [{"x":13,"y":1},{"x":13,"y":2},{"x":12,"y":3},{"x":12,"y":4}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'Camp Waterson',
+		hexes: [{"x":14,"y":1},{"x":15,"y":1},{"x":16,"y":1},{"x":14,"y":2},{"x":15,"y":2},{"x":16,"y":2},{"x":17,"y":2},{"x":15,"y":3},{"x":14,"y":3},{"x":13,"y":3},{"x":13,"y":4}],
+		buildings: ['weapon_terminal', 'ammo_tower', 'spawn_room', 'vehicle_spawn']
+		}, {
+		name: 'Nanite Overflow Station',
+		hexes: [{"x":2,"y":2},{"x":1,"y":3},{"x":2,"y":3},{"x":2,"y":4},{"x":1,"y":4}],
+		buildings: ['ammo_tower']
+		}, {
+		name: 'Benson Construction',
+		hexes: [{"x":6,"y":2},{"x":7,"y":2},{"x":6,"y":3},{"x":5,"y":3},{"x":7,"y":4},{"x":6,"y":4}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'Helios Solar, Inc.',
+		hexes: [{"x":3,"y":4},{"x":3,"y":5},{"x":2,"y":5},{"x":1,"y":5},{"x":2,"y":6},{"x":3,"y":6}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'Alkali Shipping',
+		hexes: [{"x":4,"y":4},{"x":4,"y":5},{"x":5,"y":4}],
+		buildings: ['weapon_terminal', 'ammo_tower', 'flash_spawn']
+		}, {
+		name: 'Alkali Mining Supply',
+		hexes: [{"x":8,"y":4},{"x":7,"y":5},{"x":8,"y":5},{"x":8,"y":6},{"x":9,"y":6}],
+		buildings: ['weapon_terminal', 'flash_spawn']
+		}, {
+		name: 'Crater Firing Range',
+		hexes: [{"x":9,"y":4},{"x":10,"y":4},{"x":11,"y":4},{"x":10,"y":5},{"x":9,"y":5}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'Mao',
+		hexes: [{"x":14,"y":4},{"x":15,"y":4},{"x":13,"y":5},{"x":14,"y":5},{"x":15,"y":5},{"x":14,"y":6},{"x":15,"y":6}],
+		buildings: ['weapon_terminal', 'vehicle_spawn', 'air_spawn', 'spawn_room']
+		}, {
+		name: 'NS Refinery',
+		hexes: [{"x":16,"y":4},{"x":17,"y":4},{"x":18,"y":4},{"x":16,"y":5},{"x":17,"y":5},{"x":16,"y":6},{"x":17,"y":6},{"x":16,"y":3},{"x":17,"y":3}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'Indar Excavation Site',
+		hexes: [{"x": 1, "y": 7 },{"x":2,"y":7},{"x":3,"y":7},{"x":2,"y":8},{"x":3,"y":8},{"x":4,"y":8},{"x":3,"y":9},{"x":2,"y":9},{"x":1,"y":9}],
+		buildings: ['weapon_terminal', 'vehicle_spawn', 'air_spawn', 'ammo_tower', 'spawn_room']
+		}, {
+		name: 'CoraMed Labs',
+		hexes: [{"x":4,"y":6},{"x":4,"y":7}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'Dahaka',
+		hexes: [{"x":5,"y":5},{"x":6,"y":5},{"x":5,"y":6},{"x":6,"y":6},{"x":7,"y":6},{"x":5,"y":7},{"x":6,"y":7}],
+		buildings: ['weapon_terminal', 'vehicle_spawn', 'air_spawn', 'spawn_room']
+		}, {
+		name: 'Briggs Laboratories',
+		hexes: [{"x":11,"y":5},{"x":12,"y":5},{"x":12,"y":6},{"x":13,"y":6}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'Indar Comm. Array',
+		hexes: [{"x":5,"y":8},{"x":5,"y":9},{"x":6,"y":8}],
+		buildings: ['weapon_terminal']
+		}, {
+		name: 'Seabed Listening Post',
+		hexes: [{"x":7,"y":7},{"x":8,"y":7},{"x":8,"y":8},{"x":7,"y":8},{"x":7,"y":9},{"x":8,"y":9}],
+		buildings: ['weapon_terminal']
+		}, {
+		name: 'J908 Impact Site',
+		hexes: [{"x":10,"y":6},{"x":11,"y":6},{"x":10,"y":7},{"x":9,"y":7},{"x":10,"y":8},{"x":9,"y":8}],
+		buildings: ['weapon_terminal', 'vehicle_spawn', 'air_spawn', 'spawn_room']
+		}, {
+		name: 'Galaxy Solar Plant',
+		hexes: [{"x":11,"y":7},{"x":12,"y":7},{"x":11,"y":8},{"x":12,"y":8},{"x":13,"y":8}],
+		buildings: ['weapon_terminal', 'flash_spawn']
+		}, {
+		name: 'East Canyon Checkpoint',
+		hexes: [{"x":13,"y":7},{"x":14,"y":8}],
+		buildings: ['weapon_terminal', 'flash_spawn', 'spawn_room', 'ammo_tower']
+		}, {
+		name: 'Abandoned NS Offices',
+		hexes: [{"x":14,"y":7},{"x":15,"y":7},{"x":15,"y":8}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'Howling Pass Checkpoint',
+		hexes: [{"x":18,"y":6},{"x":16,"y":7},{"x":17,"y":7},{"x":18,"y":7},{"x":19,"y":8},{x:18,y:5},{x:19,y:6},{x:17,y:8},{x:18,y:8}],
+		buildings: ['weapon_terminal']
+		}, {
+		name: 'Quartz Ridge Camp',
+		hexes: [{"x":4,"y":9},{"x":2,"y":10},{"x":3,"y":10},{"x":4,"y":10},{"x":5,"y":10},{"x":1,"y":11},{"x":2,"y":11},{"x":3,"y":11},{"x":4,"y":11},{"x":5,"y":11}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'Ceres Hydrophonics',
+		hexes: [{"x":9,"y":9},{"x":10,"y":9},{"x":11,"y":9},{"x":10,"y":10},{"x":11,"y":10}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'The Palisade',
+		hexes: [{"x":12,"y":9},{"x":13,"y":9},{"x":12,"y":10},{"x":13,"y":10},{"x":14,"y":10}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'Crimson Bluff Tower',
+		hexes: [{"x":16,"y":8},{"x":14,"y":9},{"x":15,"y":9},{"x":16,"y":9},{"x":15,"y":10},{"x":16,"y":10}],
+		buildings: ['weapon_terminal', 'vehicle_spawn', 'air_spawn', 'spawn_room', 'ammo_tower']
+		}, {
+		name: 'NS Material Storage',
+		hexes: [{"x":17,"y":9},{"x":18,"y":9},{"x":17,"y":10},{"x":18,"y":10},{x:19,y:10},{"x":18,"y":11}],
+		buildings: ['spawn_room', 'flash_spawn']
+		}, {
+		name: 'West Highlands Checkpoint',
+		hexes: [{"x":6,"y":9},{"x":6,"y":10},{"x":7,"y":10}],
+		buildings: ['spawn_room', 'flash_spawn', 'weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'Ti Alloys Inc.',
+		hexes: [{"x":8,"y":10},{"x":9,"y":10},{"x":8,"y":11},{"x":9,"y":11},{"x":9,"y":12},{"x":10,"y":12}],
+		buildings: ['flash_spawn', 'weapon_terminal']
+		}, {
+		name: 'The Crown',
+		hexes: [{"x":10,"y":11},{"x":11,"y":11},{"x":11,"y":12}],
+		buildings: ['weapon_terminal', 'vehicle_spawn', 'air_spawn', 'spawn_room', 'ammo_tower']
+		}, {
+		name: 'Zurvan',
+		hexes: [{"x":12,"y":11},{"x":13,"y":12},{"x":14,"y":12},{"x":13,"y":13},{"x":12,"y":13}],
+		buildings: ['vehicle_spawn', 'spawn_room', 'weapon_terminal']
+		}, {
+		name: "Zurvan North Camp",
+		hexes: [{"x":13,"y":11}],
+		buildings: []
+		}, {
+		name: "Zurvan West Camp",
+		hexes: [{"x":12,"y":12}],
+		buildings: []
+		}, {
+		name: 'Rust Mesa Lookout',
+		hexes: [{"x":14,"y":11},{"x":15,"y":11},{"x":15,"y":12}],
+		buildings: ['weapon_terminal']
+		}, {
+		name: 'Rashnu',
+		hexes: [{"x":16,"y":11},{"x":17,"y":11},{"x":16,"y":12},{"x":17,"y":12},{"x":18,"y":12},{"x":16,"y":13},{"x":17,"y":13}],
+		buildings: ['weapon_terminal', 'vehicle_spawn', 'air_spawn', 'spawn_room', 'ammo_tower']
+		}, {
+		name: 'Sandstone Gulch Mining',
+		hexes: [{"x":1,"y":12},{"x":0,"y":13},{"x":1,"y":14},{"x":1,"y":15},{"x":2,"y":15}],
+		buildings: ['weapon_terminal']
+		}, {
+		name: 'Hvar',
+		hexes: [{"x":2,"y":12},{"x":3,"y":12},{"x":1,"y":13},{"x":2,"y":13},{"x":3,"y":13},{"x":2,"y":14},{"x":3,"y":14}],
+		buildings: ['weapon_terminal', 'vehicle_spawn', 'air_spawn', 'spawn_room']
+		}, {
+		name: 'NS Secure Data Lab',
+		hexes: [{"x":4,"y":12},{"x":5,"y":12},{"x":4,"y":13},{"x":5,"y":13},{"x":4,"y":14},{"x":5,"y":14}],
+		buildings: ['flash_spawn', 'weapon_terminal', 'spawn_room']
+		}, {
+		name: 'Allatum',
+		hexes: [{"x":6,"y":11},{"x":7,"y":11},{"x":6,"y":12},{"x":7,"y":12},{"x":8,"y":12},{"x":6,"y":13},{"x":7,"y":13}],
+		buildings: ['spawn_room', 'weapon_terminal', 'vehicle_spawn']
+		},{
+		name: 'Indar Bay Point',
+		hexes: [{"x":3,"y":15},{"x":3,"y":16},{"x":4,"y":16},{"x":3,"y":17},{"x":4,"y":17}],
+		buildings: ['weapon_terminal', 'vehicle_spawn', 'air_spawn', 'spawn_room', 'ammo_tower']
+		}, {
+		name: 'Highlands Solar Station',
+		hexes: [{"x":4,"y":15},{"x":5,"y":15},{"x":5,"y":16}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'Vanu Archives',
+		hexes: [{"x":6,"y":14},{"x":7,"y":14},{"x":6,"y":15},{"x":7,"y":15}],
+		buildings: ['air_spawn', 'vehicle_spawn', 'ammo_tower', 'spawn_room', 'weapon_terminal']
+		}, {
+		name: 'Snake Ravine Lookout',
+		hexes: [{"x":8,"y":13},{"x":9,"y":13},{"x":8,"y":14},{"x":9,"y":14}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'Crossroads Watchtower',
+		hexes: [{"x":10,"y":13},{"x":11,"y":13},{"x":10,"y":14},{"x":11,"y":14},{"x":12,"y":14}],
+		buildings: ['weapon_terminal', 'vehicle_spawn', 'air_spawn', 'spawn_room', 'ammo_tower']
+		}, {
+		name: 'Gravel Pass',
+		hexes: [{"x":13,"y":14},{"x":12,"y":15},{"x":13,"y":15}],
+		buildings: ['flash_spawn', 'weapon_terminal']
+		}, {
+		name: 'Mesa Comm. Station',
+		hexes: [{"x":15,"y":13},{"x":14,"y":14},{"x":15,"y":14}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'Zurvan East Camp',
+		hexes: [{"x":14,"y":13}],
+		buildings: []
+		}, {
+		name: 'The Old Stockpile',
+		hexes: [{"x":18,"y":13},{"x":16,"y":14},{"x":17,"y":14},{"x":18,"y":14}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'VS Warpgate',
+		hexes: [{"x":1,"y":18},{"x":2,"y":18},{"x":3,"y":18},{"x":1,"y":19},{"x":2,"y":19},{"x":3,"y":19},{"x":2,"y":20},{"x":3,"y":20},{"x":4,"y":20}],
+		buildings: ['weapon_terminal', 'vehicle_spawn', 'air_spawn', 'spawn_room', 'ammo_tower', 'air_pad']
+		}, {
+		name: 'Ceres Biotech',
+		hexes: [{"x":4,"y":18},{"x":4,"y":19}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'Ceres Farms',
+		hexes: [{"x":5,"y":17},{"x":5,"y":18},{"x":5,"y":19}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'Ayani Labs',
+		hexes: [{"x":6,"y":16},{"x":7,"y":16}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'ARC Bioengineering',
+		hexes: [{"x":2,"y":21},{"x":3,"y":21}],
+		buildings: ['weapon_terminal']
+		}, {
+		name: 'Highlands Substation',
+		hexes: [{"x":5,"y":20},{"x":4,"y":21},{"x":5,"y":21}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'Leopardwood Nursery',
+		hexes: [{"x":6,"y":20},{"x":7,"y":20},{"x":6,"y":21},{"x":7,"y":21}],
+		buildings: ['ammo_tower']
+		}, {
+		name: 'Peris',
+		hexes: [{"x":7,"y":17},{"x":6,"y":18},{"x":7,"y":18},{"x":6,"y":19},{"x":7,"y":19},{"x":6,"y":17},{"x":8,"y":18}],
+		buildings: ['weapon_terminal', 'vehicle_spawn', 'air_spawn', 'spawn_room', 'ammo_tower']
+		}, {
+		name: 'NS Research Labs',
+		hexes: [{"x":8,"y":16}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'XenoTech Labs',
+		hexes: [{"x":8,"y":15},{"x":9,"y":15},{"x":10,"y":15},{"x":9,"y":16},{"x":10,"y":16}],
+		buildings: ['flash_spawn', 'weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'Broken Arch Road',
+		hexes: [{"x":11,"y":15},{"x":11,"y":16}],
+		buildings: ['weapon_terminal']
+		}, {
+		name: 'Platinum Mine',
+		hexes: [{"x":14,"y":15}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'Feldspar Canyon Base',
+		hexes: [{"x":15,"y":15},{"x":16,"y":15},{"x":16,"y":16},{"x":17,"y":16}],
+		buildings: ['weapon_terminal', 'vehicle_spawn', 'air_spawn', 'spawn_room', 'ammo_tower']
+		}, {
+		name: 'Valley Storage Yard',
+		hexes: [{"x":17,"y":15},{"x":18,"y":15},{"x":18,"y":16},{"x":19,"y":16},{"x":18,"y":17}, {"x": 19, "y": 17 }],
+		buildings: ['flash_spawn', 'spawn_room', 'weapon_terminal']
+		}, {
+		name: 'Iridium Mine',
+		hexes: [{"x":14,"y":16}],
+		buildings: ['weapon_terminal']
+		}, {
+		name: 'The Stronghold',
+		hexes: [{"x":15,"y":16},{"x":14,"y":17},{"x":15,"y":17}],
+		buildings: ['weapon_terminal', 'vehicle_spawn', 'spawn_room', 'ammo_tower']
+		}, {
+		name: 'Old Auraxium Mines',
+		hexes: [{"x":16,"y":17},{"x":17,"y":17}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+		}, {
+		name: 'Regent Rock Garrison',
+		hexes:[{"x":8,"y":17},{"x":9,"y":17},{"x":10,"y":17},{"x":9,"y":18},{"x":10,"y":18},{"x":11,"y":18}],
+		buildings: ['weapon_terminal', 'vehicle_spawn', 'air_spawn', 'spawn_room', 'ammo_tower']
+		}, {
+		name: 'Tawrich',
+		hexes: [{"x":12,"y":16},{"x":13,"y":16},{"x":11,"y":17},{"x":12,"y":17},{"x":13,"y":17},{"x":12,"y":18},{"x":13,"y":18}],
+		buildings: ['weapon_terminal', 'vehicle_spawn', 'air_spawn', 'spawn_room', 'ammo_tower']
+		}, {
+		name: 'Scarred Mesa Skydock',
+		hexes: [{"x":10,"y":19},{"x":11,"y":19},{"x":11,"y":20},{"x":10,"y":20},{"x":9,"y":21},{"x":10,"y":21}],
+		buildings: ['weapon_terminal', 'air_spawn']
+		}, {
+		name: 'Red Ridge Communications',
+		hexes: [{"x":12,"y":19},{"x":12,"y":20},{"x":13,"y":20},{x:11,y:21},{x:12,y:21}],
+		buildings: ['flash_spawn', 'air_pad']
+		}, {
+		name: 'Spec-Ops Training Camp',
+		hexes: [{"x":14,"y":18},{"x":13,"y":19},{"x":14,"y":19}],
+		buildings: ['weapon_terminal']
+		}, {
+		name: 'Arroyo Torre Station',
+		hexes: [{"x":15,"y":18},{"x":16,"y":18},{"x":17,"y":18}],
+		buildings: ['weapon_terminal', 'vehicle_spawn', 'air_spawn', 'spawn_room', 'ammo_tower']
+		}, {
+		name: 'NC Warpgate',
+		hexes: [{"x":18,"y":18},{"x":19,"y":18},{"x":20,"y":18},{"x":17,"y":19},{"x":18,"y":19},{"x":19,"y":19},{"x":17,"y":20},{"x":18,"y":20},{"x":19,"y":20}],
+		buildings: ['weapon_terminal', 'vehicle_spawn', 'air_spawn', 'spawn_room', 'ammo_tower', 'air_pad']
+		}, {
+		name: 'Lost End Outlook',
+		hexes: [{"x":14,"y":20},{"x":15,"y":20},{"x":14,"y":21},{"x":13,"y":21},{"x":15,"y":21}],
+		buildings: ['air_pad']
+		}, {
+		name: 'Copper Ravine Station',
+		hexes: [{"x":15,"y":19},{"x":16,"y":19},{"x":16,"y":20},{"x":16,"y":21},{"x":17,"y":21},{"x":18,"y":21}],
+		buildings: ['weapon_terminal', 'ammo_tower']
+	}
+
+
+
+];
+
+

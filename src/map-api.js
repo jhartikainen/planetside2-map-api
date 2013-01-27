1be80b69c3cf6d6592305572c3040aebaa8dfb43
Jani Hartikainen <jani@jjh.fi>
2013-01-27 09:02:46 +0200

    Labeling for grid and fix grid to match ingame grid

--- src/map-api.js
+++ src/map-api.js
@@ -491,6 +491,7 @@ ps2hq.map.GridLayer = L.Class.extend({
 	includes: L.Mixin.Events,
 
 	_fg: null,
+	_lg: null,
 	_lineStyles: null,
 
 	initialize: function(lineStyles) {
@@ -501,17 +502,31 @@ ps2hq.map.GridLayer = L.Class.extend({
 	},
 
 	onAdd: function(map) {
-		if(this._fg) {
-			this._map.removeLayer(this._fg);
-		}
+		this.onRemove();
 
 		this._map = map;
 
 		var latlngs = [];
-		for(var i = 0; i <= 8; i++) {
+		var aCode = 'A'.charCodeAt(0);
+		var row = 1;
+		var labels = [];
+		for(var i = 0; i <= 8; i += 0.5) {
 			latlngs.push([new L.LatLng(i, 0), new L.LatLng(i, 8)]);
 			latlngs.push([new L.LatLng(0, i), new L.LatLng(8, i)]);
+
+			if(row < 17) {
+				var icon = L.divIcon({ className: 'sector-name', html: row });
+				var marker = L.marker(new L.LatLng(i + 0.25, -0.25), { icon: icon, clickable: false });
+				labels.push(marker);
+
+				var icon = L.divIcon({ className: 'sector-name', html: String.fromCharCode(aCode + row - 1) });
+				var marker = L.marker(new L.LatLng(-0.25, i + 0.25), { icon: icon, clickable: false });
+				labels.push(marker);
+			}
+			row++;
 		}
+
+		this._lg = new L.LayerGroup(labels);
 
 		this._fg = new L.MultiPolyline(latlngs, L.Util.extend({
 			color: 'white',
@@ -520,6 +535,7 @@ ps2hq.map.GridLayer = L.Class.extend({
 		}, this._lineStyles));
 
 		this._map.addLayer(this._fg);
+		this._map.addLayer(this._lg);
 	},
 
 	onRemove: function(map) {
@@ -527,7 +543,12 @@ ps2hq.map.GridLayer = L.Class.extend({
 			this._map.removeLayer(this._fg);
 		}
 
+		if(this._lg) {
+			this._map.removeLayer(this._lg);
+		}
+
 		this._fg = null;
+		this._lg = null;
 		this._map = null;
 	},
 
